767324009a42d8ba2d225b61eda7403d10dfb13f
Wahyu Bagus Pambudi <wb84222@gmail.com>
2025-01-17 14:29:10 +0700

    feat: :sparkles: `Update dependencies and add new scripts to _document.tsx`

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,9 +4,21 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <link
+        href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css"
+        rel="stylesheet"
+      />
       <body>
         <Main />
         <NextScript />
+        <script src="/assets/js/bootstrap.bundle.min.js" defer />
+        <script src="/assets/js/scripts.js" defer />
+        <script
+          src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js"
+          crossOrigin="anonymous"
+          defer
+        />
+        <script src="/assets/js/datatables-simple-demo.js" defer />
       </body>
     </Html>
   );
